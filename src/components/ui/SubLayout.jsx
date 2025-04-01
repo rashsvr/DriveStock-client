5faa7b826313a105394a5c062d48123659a7ee22
rashsvr <rashmi.s.v.rajapaksha@gmail.com>
2025-04-01 12:52:27 +0530

    responsive  sublayout

--- src/components/ui/SubLayout.jsx
+++ src/components/ui/SubLayout.jsx
@@ -16,26 +16,46 @@ const SubLayout = ({ title, children }) => {
     };
 
     return (
-        <div >
+        <div className="relative min-h-screen">
             {isProcessing && <LoadingAnimation />}
 
-            <div className="grid grid-cols-12 grid-rows-9 gap-0">
-                <div className="col-span-2 col-start-2 row-start-2"><Title text={title} /></div>
-                <div className="col-span-4 col-start-8 row-start-2"><SearchBar /></div>
-                <div className="col-span-5 col-start-2 row-start-3"><Breadcrumbs /></div>
-                <div className="col-span-6 col-start-2 row-start-4"><FilterChips/></div>
-                <div className="col-span-10 col-start-2 row-start-5"><FilterChips/></div>
-                <div className="col-span-8 row-span-6 col-start-3 row-start-6">
-                    <ProductCard />
-
-            {children}
+            <div className="grid grid-cols-12 gap-4 px-4 sm:px-6 lg:px-0">
+                {/* Title */}
+                <div className="col-span-12 sm:col-span-6 lg:col-span-2 lg:col-start-2 lg:row-start-2">
+                    <Title text={title} />
                 </div>
-                <div className="col-span-8 col-start-3 row-start-12 text-center underline text-orange-500">
+
+                {/* SearchBar */}
+                <div className="col-span-12 sm:col-span-6 lg:col-span-4 lg:col-start-8 lg:row-start-2">
+                    <SearchBar />
+                </div>
+
+                {/* Breadcrumbs */}
+                <div className="col-span-12 lg:col-span-5 lg:col-start-2 lg:row-start-3">
+                    <Breadcrumbs />
+                </div>
+
+                {/* FilterChips (Top) */}
+                <div className="col-span-12 lg:col-span-6 lg:col-start-2 lg:row-start-4">
+                    <FilterChips />
+                </div>
+
+                {/* FilterChips (Secondary) */}
+                <div className="col-span-12 lg:col-span-10 lg:col-start-2 lg:row-start-5">
+                    <FilterChips />
+                </div>
+
+                {/* ProductCard and Children */}
+                <div className="col-span-12 lg:col-span-8 lg:row-span-6 lg:col-start-3 lg:row-start-6">
+                    <ProductCard />
+                    {children}
+                </div>
+
+                {/* Load More */}
+                <div className="col-span-12 lg:col-span-8 lg:col-start-3 lg:row-start-12 text-center underline text-orange-500">
                     <h1>Load more</h1>
                 </div>
-    
             </div>
-
         </div>
     );
 };
